9eec02f27aade927c09cd46d4f78ef52f46e36a0
chichi <yangju058@gmail.com>
2024-07-15 19:46:43 +0900

    (2024.7.15) 삽입 정렬

---
+++ Program/Class3th (Bubble Sort)/Source.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#define SIZE 5
+
+using namespace std;
+
+int list[SIZE] = { 15,25,35,5,20 };
+
+void Number()
+{
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		for (int j = 0; j < (SIZE - i) - 1; j++)
+		{
+			if (list[j] > list[j + 1])
+			{
+				std::swap(list[j], list[j + 1]);
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+
+	for (const int& element : list)
+	{
+		cout << element << " ";
+	}
+
+	//for (int i = 0; i < SIZE; i++)
+	//{
+	//	cout << list[i] << " ";
+	//}
+}
+
+
+
+int main()
+{
+#pragma region 거품 정렬
+	// 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘입니다.
+
+	Number();
+
+#pragma endregion
+
+
+
+	return 0;
+}
---
+++ Program/Class4th ( Select Sort)/Source.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#define SIZE 5
+
+using namespace std;
+
+int list[SIZE] = { 3, 6, 7, 9, 5 };
+
+// 출력값: 3 5 6 7 9 나와야함
+void Select()
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		int min = list[i];
+
+		int select = i;
+
+		for (int j = i + 1; j < SIZE; j++)
+		{
+			if (min > list[j])
+			{
+				min = list[j];
+				select = j;
+			}
+		}
+		swap(list[i], list[select]);
+	}
+
+	for (const int& element : list)
+	{
+		cout << element << " ";
+
+	}
+	//for (int i = 0; i < SIZE; i++)
+	//{
+	//	cout << list[i] << " ";
+	//}
+}
+
+
+
+int main()
+{
+#pragma region  선택 정렬
+	// 주어진 리스트 중에 최소값을 찾아서 맨 앞에 위치한
+	// 결과를 교체하는 방식으로 정렬하는 알고리즘입니다.
+
+	Select();
+
+
+#pragma endregion
+
+
+	return 0;
+}

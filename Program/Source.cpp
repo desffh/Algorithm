ac32a4d699ddcdf01af4ddf2da2ab9297f8b861f
chichi <yangju058@gmail.com>
2024-07-22 19:47:21 +0900

    (2024.7.22) 탐욕법

--- Program/Source.cpp
+++ Program/Source.cpp
@@ -1,47 +1,28 @@
 ﻿#include <iostream>
+#define SIZE 7
 
-using namespace std;
+ using namespace std;
 
-#define SIZE 8
-
-// 피보나치수열 배열에 값 넣기
-int Fibonacci(int list[], int n)
-{
-
-    if (n <= 0)
-    {
-        return 0;
-    }
-
-    else if (n == 1)
-    {
-        return 1;
-    }
-
-    else
-    {
-        return Fibonacci(list,n - 1) + Fibonacci(list,n - 2);
-    }
-
-}
 
 int main()
 {
-#pragma region 동적 계획법
-    // 특정 범위까지의 값을 구하기 위해서 그것과
-    // 다른 범위까지의 값을 이용하여 효율적으로 값을
-    // 구하는 알고리즘입니다.
+#pragma region 쉘 정렬
+    // 먼 거리의 요소들을 먼저 정렬하여 배열을 부분적으로
+    // 정렬한 후, 점진적으로 더 작은 간격을 사용하는 정렬 알고리즘입니다.
 
-    // 메모이제이션
-    // 프로그램이 동일한 계산을 반복해야 할 때,
-    // 이전에 계산한 값을 메모리에 저장함으로써 동일한 계산을 반복 수행하는
-    // 작업을 제거하여 프로그램의 실행 속도를 향상시키는 기법입니다.
+    int list[SIZE] = { 10, 8, 6, 20, 4,3, 22 };
 
-    // 숫자가 커지면 계산시간이 오래걸림(시간복잡도n²)
+    int gap = SIZE / 2;
 
-    cout << Fibonacci(100) << endl;
+    for (int i = 0; i < SIZE; i++)
+    {
+        
+    }
+
+
 
 #pragma endregion
+
 
     return 0;
 }

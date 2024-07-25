d6b72d5bc90b01776bcf9c767e4cd2b0f2f14f13
chichi <yangju058@gmail.com>
2024-07-25 19:46:29 +0900

    (2024.7.25) 깊이 우선 탐색

---
+++ Program/Class15th (Back Tracking)/Source.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#define SIZE 11
+using namespace std;
+
+void HanoiTower(int n, char from, char by, char to)
+{
+    if (n == 1)
+    {
+        cout << "[" << n << "]" << from << "->" << to << endl;
+    }
+    else
+    {
+        HanoiTower(n - 1, from, to, by);
+        cout << "[" << n << "]" << from << "->" << to << endl;
+        HanoiTower(n - 1, by, from, to);
+    }
+
+}
+
+
+int main()
+{
+#pragma region 백트래킹
+    // 해를 찾아가는 도중에 지금의 경로가 해가 될 것 같지 않으면,
+    // 더 이상 깊이 들어가지 않고, 이전 단계로 다시 돌아가는 알고리즘입니다.
+
+    // 백트래킹의 유망성 판단
+    // 해가 될 만한지 판단한 후에 유망하지 않다고 결정되면,
+    // 그 노드의 이전 노드로 돌아가 다음 자식 노드로 이동합니다.
+
+    HanoiTower(3, 'A', 'B', 'C');
+#pragma endregion
+
+
+    return 0;
+}
--- Program/Source.cpp
+++ Program/Source.cpp
@@ -1,18 +1,17 @@
 ﻿#include <iostream>
-#define SIZE 11
+
+
 using namespace std;
 
-void hanoi(int n, char from, char by, char to)
-{
 
-}
+
 
 
 int main()
 {
-#pragma region 백 트래킹
-    
-#pragma endregion
+
+
+
 
 
     return 0;

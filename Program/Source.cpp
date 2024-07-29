93c088f9addccc5c637627e8277ffc4a5e8e9677
chichi <yangju058@gmail.com>
2024-07-29 19:46:37 +0900

    (2024.7.29) 유니온 파인드

--- Program/Source.cpp
+++ Program/Source.cpp
@@ -1,105 +1,78 @@
 ﻿#include <iostream>
-#include <queue>
-#include <vector>
 
-#define SIZE 8
+#define SIZE 7
 using namespace std;
-
 
 class Graph
 {
 private:
-    int degree[SIZE];
-
-    queue <int> queue; // Queue 배열
-
-    vector <int> graph[SIZE]; // 인접 리스트
+    int parent[SIZE];
 
 public:
-    Graph() // 생성자에 배열 초기화
+    Graph()
     {
-        for (int i = 0; i < SIZE; i++)
+        for (int i = 1; i < SIZE; i++)
         {
-            degree[i] = false;
+            parent[i] = i;
         }
     }
 
-    void Insert(int vertex, int edge)
+    int Find(int x)
     {
-        graph[vertex].push_back(edge);
+        for (int i = 1; i < SIZE; i++)
+        {
+            if (parent[x] == i)
+            {
+                return parent[x];
+            }
+        }
     }
 
-    void Search(int start)
+    int Union(int x, int y)
     {
-        queue.push(start);
-        degree[start] = true;
-
-        while (queue.empty() == false)
+        while (parent[x] != parent[y])
         {
-            int x = queue.front();
-            queue.pop();
-
-            cout << x << " ";
-
-            for (int i = 0; i < graph[x].size(); i++)
+            Find(x);
+            Find(y);
+            if (parent[x] < parent[y])
             {
-                int next = graph[x][i]; // 2개 넣는거임
-
-                if (degree[next] == false)
-                {
-                    queue.push(next);
-                    degree[next] = true;
-                }
+                parent[y] = parent[x];
             }
+            else
+            {
+                parent[x] = parent[y];
+            }
+        }
+    }
+
+    bool Same(int x, int y)
+    {
+        if (Find(x) == Find(y))
+        {
+            return Find(x);
+            return Find(y);
         }
     }
 };
 
+
 int main()
 {
-#pragma region 위상 정렬
-    // 방향 그래프에 존재하는 각 정점들의 선행 순서를 지키며,
-    // 모든 정점을 차례대로 진행하는 알고리즘입니다.
+#pragma region 유니온 파인드
+    // 여러 노드가 존재할 때 어떤 노드가 다른 노드와 연결되어
+    // 있는지 확인하는 알고리즘입니다.
 
-    // 사이클이 발생하는 경우 위상 정렬을 수행할 수 없습니다.
-    
-    // DAG(Directed Acyclic Graph): 사이클이 존재하지 않는 그래프
-
-    // 시간 복잡도: O(V + E)
-
-    // 위상 정렬하는 방법
-
-    // 1. 진입 차수가 0인 정점을 Queue에 삽입합니다.
-
-    // 2. Queue에서 원소를 꺼내 연결된 모든 간선을 제거합니다.
-
-    // 3. 간선 제거 이후에 진입 차수가 0이 된 정점을 Queue에 삽입합니다.
-
-    // 4. Queue가 비어있을 때까지 2번 ~ 3번을 반복 수행합니다. 
+    // 유니온 파인드의 시간 복잡도
+    // O(MlogN) : M은 연산의 개수, N은 노드의 개수
+    // M이 N²에 가까울 때는 O(N²logN)이 됩니다.
 
     Graph graph;
 
-    graph.Insert(1, 2);
-    graph.Insert(1, 5);
+    graph.Union(1, 2);
 
-    graph.Insert(2, 3);
-    graph.Insert(3, 4);
-    graph.Insert(4, 7);
-
-    graph.Insert(5, 6);
-    graph.Insert(6, 4);
-
-    graph.Search(1);
-
-
-
-
-
-
-
+    graph.Same(1, 2);
 
 #pragma endregion
-
 
 
 

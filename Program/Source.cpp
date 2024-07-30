fe0db4f5e64f9ff554c0d1d91f9188699f7dfa1b
chichi <yangju058@gmail.com>
2024-07-30 19:46:37 +0900

    (2024.7.30) 최소 신장 트리

--- Program/Source.cpp
+++ Program/Source.cpp
@@ -1,13 +1,54 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm>
 
-#define SIZE 7
+
+#define SIZE 8
 using namespace std;
 
 class Graph
 {
 private:
+
+    class Edge
+    {
+    private:
+        int x;
+        int y;
+        int distance;
+
+    public:
+        Edge(int x, int y, int distance)
+        {
+            this->x = x;
+            this->y = y;
+            this->distance = distance;
+        }
+        
+        int& X()
+        {
+            return x;
+        }
+        int& Y()
+        {
+            return y;
+        }
+        int& Distance()
+        {
+            return distance;
+        }
+
+        bool operator < (Edge edge)
+        {
+            return this->distance < edge.distance;
+        }
+    };
+    
+    vector <Edge> graph; // Edge를 담을 vector배열
+
     int parent[SIZE];
 
+    int sum;
 public:
     Graph()
     {
@@ -17,63 +58,84 @@ public:
         }
     }
 
+    void Insert(int x, int y, int distance)
+    {
+        graph.push_back(Edge(x,y,distance));
+    }
+
     int Find(int x)
     {
-        for (int i = 1; i < SIZE; i++)
+        // 부모노드 찾기
+        if (parent[x] == x)
         {
-            if (parent[x] == i)
-            {
-                return parent[x];
-            }
+            return x;
+        }
+        else
+        {
+            parent[x] = Find(parent[x]);
+            return parent[x];
         }
     }
 
-    int Union(int x, int y)
+    void Union(int x, int y)
     {
-        while (parent[x] != parent[y])
+        x = Find(x);
+        y = Find(y);
+
+        if (x < y)
         {
-            Find(x);
-            Find(y);
-            if (parent[x] < parent[y])
-            {
-                parent[y] = parent[x];
-            }
-            else
-            {
-                parent[x] = parent[y];
-            }
+            parent[y] = x;
+
+        }
+        else
+        {
+            parent[x] = y;
         }
     }
 
-    bool Same(int x, int y)
+    void Kruskal()
     {
-        if (Find(x) == Find(y))
-        {
-            return Find(x);
-            return Find(y);
-        }
+        int sum = 0;
+
+
     }
+
+    
+
 };
+
 
 
 int main()
 {
-#pragma region 유니온 파인드
-    // 여러 노드가 존재할 때 어떤 노드가 다른 노드와 연결되어
-    // 있는지 확인하는 알고리즘입니다.
+#pragma region 신장 트리
+    // 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
+    // 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
 
-    // 유니온 파인드의 시간 복잡도
-    // O(MlogN) : M은 연산의 개수, N은 노드의 개수
-    // M이 N²에 가까울 때는 O(N²logN)이 됩니다.
+    // 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개입니다.
+
+    // 최소 비용 신장 트리
+    // 그래프의 간선들의 가중치 합이 최소인 신장 트리 
 
     Graph graph;
+    
+    graph.Insert(1, 7, 10);
+    graph.Insert(1, 4, 28);
+    graph.Insert(1, 2, 66);
+    graph.Insert(1, 5, 19);
 
-    graph.Union(1, 2);
+    graph.Insert(2, 4, 25);
+    graph.Insert(2, 5, 60);
 
-    graph.Same(1, 2);
+    graph.Insert(3, 5, 21);
+    graph.Insert(3, 6, 35);
+
+    graph.Insert(4, 7, 15);
+    graph.Insert(5, 6, 40);
+
+
 
 #pragma endregion
-
 
 
 

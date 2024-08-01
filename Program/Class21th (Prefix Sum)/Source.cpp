b1741612deea2bb56f5c1a745f5fc4700ad3dfd3
desffh <163123142+desffh@users.noreply.github.com>
2024-08-01 19:46:18 +0900

    (2024.8.1) 다익스트라 알고리즘

---
+++ Program/Class21th (Prefix Sum)/Source.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+#define SIZE 5
+using namespace std;
+
+
+
+int main()
+{
+#pragma region 누적합
+    // 특정한 배열이 있을 때, 배열까지의 합을 나타내는 것입니다.
+
+    int array[SIZE] = { 1,2,3,4,5 };
+
+    int save[SIZE] = { 0,0,0,0,0 };
+
+    save[0] = array[0];
+
+    for (int i = 1; i < SIZE; i++)
+    {
+        save[i] = array[i] + save[i - 1];
+    }
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << save[i] << " ";
+    }
+
+
+#pragma endregion
+
+
+
+
+    return 0;
+}
